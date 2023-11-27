ad1c317d37521112af6007abb3ca81ccb9f18c66
Marc Chen <marcchen955@gmail.com>
2023-11-27 08:38:19 +0800

    frontend basic

--- apps/electron-frontend/src/pages/tabs.tsx
+++ apps/electron-frontend/src/pages/tabs.tsx
@@ -1,7 +1,56 @@
+import { Button, Tabs } from 'antd';
+import { useRef, useState } from 'react';
+import styles from  "@/styles/tab-manager.module.scss";
+const defaultPanes = new Array(2).fill(null).map((_, index) => {
+  const id = String(index + 1);
+  return { label: `Tab ${id}`, children: `Content of Tab Pane ${index + 1}`, key: id };
+});
+
+type TargetKey = React.MouseEvent | React.KeyboardEvent | string;
+
 export default function WindowTabManager() {
+  const [activeKey, setActiveKey] = useState(defaultPanes[0].key);
+  const [items, setItems] = useState(defaultPanes);
+  const newTabIndex = useRef(0);
+
+  const onChange = (key: string) => {
+    setActiveKey(key);
+  };
+
+  const add = () => {
+    const newActiveKey = `newTab${newTabIndex.current++}`;
+    setItems([...items, { label: 'New Tab', children: 'New Tab Pane', key: newActiveKey }]);
+    setActiveKey(newActiveKey);
+  };
+
+  const remove = (targetKey: TargetKey) => {
+    const targetIndex = items.findIndex((pane) => pane.key === targetKey);
+    const newPanes = items.filter((pane) => pane.key !== targetKey);
+    if (newPanes.length && targetKey === activeKey) {
+      const { key } = newPanes[targetIndex === newPanes.length ? targetIndex - 1 : targetIndex];
+      setActiveKey(key);
+    }
+    setItems(newPanes);
+  };
+
+  const onEdit = (targetKey: TargetKey, action: 'add' | 'remove') => {
+    if (action === 'add') {
+      add();
+    } else {
+      remove(targetKey);
+    }
+  };
+
   return (
-    <div className="window-tabs-manager">
-      Tabs
+    <div className={styles.tabManager}>
+      <Tabs
+        hideAdd
+        onChange={onChange}
+        activeKey={activeKey}
+        type="editable-card"
+        onEdit={onEdit}
+        items={items}
+      />
     </div>
   );
 }
