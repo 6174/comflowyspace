352908094845561ca6ec5f74fed79fbb3d04d3bd
Marc Chen <marcchen955@gmail.com>
2023-12-20 17:45:56 +0800

    menu

--- apps/electron-frontend/src/components/layout/layout.tsx
+++ apps/electron-frontend/src/components/layout/layout.tsx
@@ -8,6 +8,8 @@ type Props = {
 }
 import styles from "./layout.style.module.scss";
 import { useRouter } from 'next/router'
+import LogoIcon from 'ui/icons/logo'
+import { BulbIcon, ExtensionIcon, ModelIcon, WorkflowIcon } from 'ui/icons'
 const Layout = ({ children, title = 'This is the default title' }: Props) => {
   return (
     <>
@@ -31,23 +33,40 @@ const WorkspaceNav = () => {
   const path = route.pathname;
   return (
     <div className="workspace-nav">
+      <div className="logo">
+        <LogoIcon/>
+        <div className="text">Comflowy</div>
+      </div>
+      <div className="sub">MENU</div>
       <div className="nav-list">
         <div className={`workspace-nav-item ${path === "/" ? "active" : ""}`}>
+          <div className="icon">
+            <WorkflowIcon/>
+          </div>
           <Link href="/">
             My workflows
           </Link>
         </div>
         <div className={`workspace-nav-item ${path === "/templates" ? "active" : ""}`}>
+          <div className="icon">
+            <BulbIcon/>
+          </div>
           <Link href="/templates">
             Templates
           </Link>
         </div>
         <div className={`workspace-nav-item ${path === "/models" ? "active" : ""}`}>
+          <div className="icon">
+            <ModelIcon/>
+          </div>
           <Link href="/models">
             Models
           </Link>
         </div>
         <div className={`workspace-nav-item ${path === "/extensions" ? "active" : ""}`}>
+          <div className="icon">
+            <ExtensionIcon/>
+          </div>
           <Link href="/extensions">
             Extensions
           </Link>
