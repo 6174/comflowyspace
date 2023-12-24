64d495f67f6afe185b7a5bc3afca7e641d3ef3a0
Marc Chen <marcchen955@gmail.com>
2023-12-24 11:30:36 +0800

    bugfix bootstrap get widgets

--- apps/electron-frontend/src/pages/index.tsx
+++ apps/electron-frontend/src/pages/index.tsx
@@ -4,20 +4,22 @@ import MyWorkflow from "@/components/my-workflows/my-workflows";
 import { useAppStore } from "@comflowy/common/store";
 import { useDashboardState } from "@comflowy/common/store/dashboard-state";
 import { useEffect } from "react";
+
 export default function WorkspaceHome() {
   const {bootstraped} = useDashboardState();
+  
   const {onInit} = useAppStore();
+    useEffect(() => {
+    if (typeof window !== 'undefined' && bootstraped) {
+      onInit();
+    }
+  }, [bootstraped]);
+
   if (!bootstraped) {
     return (
       <Bootstrap/>
     )
   }
-
-  useEffect(() => {
-    if (bootstraped) {
-      onInit();
-    }
-  }, [bootstraped]);
 
   return (
     <Layout title="Comflowy - Workspace Home">
