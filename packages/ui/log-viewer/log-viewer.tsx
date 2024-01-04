0a8919ebb63d2965f82d498706588f2e3b36ef1c
6174 <marcchen955@gmail.com>
2024-01-04 14:56:59 +0800

    show progress oneline log on bootstrap

--- packages/ui/log-viewer/log-viewer.tsx
+++ packages/ui/log-viewer/log-viewer.tsx
@@ -1,19 +1,31 @@
 import styles from "./log-viewer.style.module.scss";
 export type LogViewerProps = {
-  messages: string[]
+  messages: string[],
+  oneline: boolean
 }
 export function LogViewer(props: LogViewerProps) {
-  const {messages = []} = props;
+  const {messages = [], oneline} = props;
   if (messages.length === 0) {
     return null;
   }
+
   return (
-    <div className={styles.logViewer}>
-      {props.messages.map((message, i) => (
-        <div key={i} className="log-viewer-message">
-          {message}
-        </div>
-      ))}
+    <div className={styles.logViewer + " " + oneline ? styles.oneline : ""}>
+      {
+        oneline ? (
+          <div key="oneline" className="log-viewer-message">
+            {messages[messages.length - 1]}
+          </div>
+        ) : (
+          <div className="message-list">
+            {messages.map((message, i) => (
+              <div key={i} className="log-viewer-message">
+                {message}
+              </div>
+            ))}
+          </div>
+        )
+      }
     </div>
   )
 }
