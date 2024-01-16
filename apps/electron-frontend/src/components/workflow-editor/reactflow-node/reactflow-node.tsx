edcb7abc54335447e5a2cc386307081d4f5f8e1c
6174 <marcchen955@gmail.com>
2024-01-16 15:42:40 +0800

    image node blink problem ,fix https://github.com/6174/comflowyspace/issues/115

--- apps/electron-frontend/src/components/workflow-editor/reactflow-node/reactflow-node.tsx
+++ apps/electron-frontend/src/components/workflow-editor/reactflow-node/reactflow-node.tsx
@@ -248,7 +248,7 @@ export const NodeComponent = memo(({
             <div className="inner">
               <Image.PreviewGroup>
               {
-                imagePreviews && imagePreviews.reverse().map((image, index) => {
+                imagePreviews && imagePreviews.map((image, index) => {
                   const imageSrc = getImagePreviewUrl(image.filename, image.type, image.subfolder)
                   return (
                     <ImageWithDownload
