9b30e4730a2c00bdd3992a364f84f2d4fcb1826e
6174 <marcchen955@gmail.com>
2024-01-17 11:43:23 +0800

    extension can listen to editorEvent

--- apps/electron-frontend/src/components/workflow-editor/websocket-controller/websocket-controller.tsx
+++ apps/electron-frontend/src/components/workflow-editor/websocket-controller/websocket-controller.tsx
@@ -5,6 +5,7 @@ import { useAppStore } from '@comflowy/common/store';
 import { useEffect, useState } from 'react';
 import {useQueueState} from '@comflowy/common/store/comfyui-queue-state';
 import { GlobalEvents, SlotGlobalEvent } from '@comflowy/common/utils/slot-event';
+import { ComfyUIEvents } from '@comflowy/common/comfui-interfaces/comfy-event-types';
 export function WsController(): JSX.Element {
   const clientId = useAppStore((st) => st.clientId);
   const nodeInProgress = useAppStore((st) => st.nodeInProgress);
@@ -12,7 +13,7 @@ export function WsController(): JSX.Element {
   const onQueueUpdate = useQueueState((st) => st.onQueueUpdate);
   const onNodeInProgress = useAppStore((st) => st.onNodeInProgress);
   const onImageSave = useAppStore((st) => st.onImageSave);
-
+  const editorEvent = useAppStore(st => st.editorEvent);
   const nodeIdInProgress = nodeInProgress?.id;
 
   const [socketUrl, setSocketUrl] = useState(`ws://${config.host}/comfyui/ws`);
@@ -21,6 +22,10 @@ export function WsController(): JSX.Element {
     queryParams: clientId ? { clientId } : {},
     onMessage: (ev) => {
       const msg = JSON.parse(ev.data)
+      editorEvent.emit({
+        type: ComfyUIEvents.RunMessage,
+        data: msg
+      });
       // console.log("msg", msg)
       if (Message.isStatus(msg)) {
         if (msg.data.sid !== undefined && msg.data.sid !== clientId) {
