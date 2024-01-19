14dc7ef8c6f86743b43e77f16bca68b4c98ffd94
6174 <marcchen955@gmail.com>
2024-01-19 22:52:06 +0800

    turn bootstrap module into shell script

---
+++ apps/node/src/modules/comfyui/bootstrap.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+CONDA_ENV_NAME="comflowy" # 定义conda环境名
+
+# 检查命令是否已经安装
+is_installed() {
+    command -v $1 >/dev/null 2>&1
+    return $?
+}
+
+# 通过conda安装Python
+install_python() {
+    if conda env list | grep -q "^${CONDA_ENV_NAME}.*"; then
+        echo "Python=3.10.8 already installed in Conda environment ${CONDA_ENV_NAME}"
+        return 0
+    fi
+
+    echo "Start installing Python=3.10.8 in conda..."
+    conda create -n ${CONDA_ENV_NAME} python=3.10.8 -y
+    if [ $? -eq 0 ]; then
+        echo "Installed Python=3.10.8 successfully."
+    else
+        echo "Failed to install Python=3.10.8."
+        return 1
+    fi
+}
+
+# 安装conda
+install_conda() {
+    if is_installed conda; then
+        echo "conda already installed"
+        install_python
+        return $?
+    fi
+
+    echo "Installing conda..."
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        ARCHITECTURE=$(uname -m)
+        if [[ "$ARCHITECTURE" == "arm64" ]]; then
+            wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh -O miniconda.sh
+        else
+            wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh
+        fi
+
+        bash miniconda.sh -b
+        if [ $? -eq 0 ]; then
+            echo "Installed conda successfully."
+        else
+            echo "Failed to install conda."
+            return 1
+        fi
+
+        install_python
+        return $?
+    else
+        echo "Unsupported OS"
+        return 1
+    fi
+}
+
+# 运行函数
+install_conda
