93e0c88912f2b85305b9e3e33ab366d86a22e7c1
6174 <marcchen955@gmail.com>
2024-01-21 11:12:36 +0800

    autoupdate error log actch

--- apps/electron-backend/layers/main/src/auto-update.ts
+++ apps/electron-backend/layers/main/src/auto-update.ts
@@ -1,19 +1,17 @@
 import { app, dialog, MessageBoxOptions } from "electron";
-import { autoUpdater, UpdateDownloadedEvent } from 'electron-updater';
+import { autoUpdater, UpdateDownloadedEvent, UpdateInfo } from 'electron-updater';
 import isDev from "electron-is-dev";
 import log from 'electron-log';
+import logger from "@comflowy/node/src/modules/utils/logger";
 
 export function startAutoUpdater() {
     if (require('electron-squirrel-startup')) {
         app.quit();
     }
     if (!isDev) {
-        setInterval(() => {
-            autoUpdater.checkForUpdates();
-        }, 60000);
-
-        autoUpdater.on('update-available', () => {
+        autoUpdater.on('update-available', (info: UpdateInfo) => {
             log.debug('Update available.');
+            logger.info("update-available: " + JSON.stringify(info));
         });
 
         autoUpdater.on('update-downloaded', (info: UpdateDownloadedEvent) => {
@@ -33,8 +31,22 @@ export function startAutoUpdater() {
         });
 
         autoUpdater.on('error', (err) => {
-            log.error('There was a problem updating the application');
-            log.error(err);
+            logger.error('There was a problem updating the application: ' + err.message);
         });
+
+        try {
+            autoUpdater.checkForUpdates();
+            setInterval(() => {
+                autoUpdater.checkForUpdates();
+            }, 60000);
+        } catch(err: any) {
+            logger.error("auto update error: " + err.message)
+            // dialog.showMessageBox({
+            //     type: 'info',
+            //     title: 'Fetch new version failed',
+            //     message: 'Error: ' + err.message,
+            //     buttons: ['OK']
+            // })
+        }
     }
 }
