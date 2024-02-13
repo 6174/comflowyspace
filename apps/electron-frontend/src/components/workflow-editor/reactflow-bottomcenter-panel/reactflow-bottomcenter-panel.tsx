90dce4a75a0b3f4ae08c486c3b67995761627f88
6174 <marcchen955@gmail.com>
2024-02-13 14:33:19 +0800

    add run event

--- apps/electron-frontend/src/components/workflow-editor/reactflow-bottomcenter-panel/reactflow-bottomcenter-panel.tsx
+++ apps/electron-frontend/src/components/workflow-editor/reactflow-bottomcenter-panel/reactflow-bottomcenter-panel.tsx
@@ -7,11 +7,13 @@ import { ExtensionIcon, PlusIcon, SelectionIcon, StartIcon } from "ui/icons";
 import { ImageIcon, ModelIcon, PromptIcon, SamplerIcon, VaeIcon, WIDGET_ICONS, getWidgetIcon } from "../reactflow-node/reactflow-node-icons";
 import { Widget } from "@comflowy/common/comfui-interfaces";
 import { ExtensionListPopover } from "@/lib/extensions/extensions-list-popover";
+import { useAptabase } from "@aptabase/react";
 
 function ReactflowBottomCenterPanel() {
     const selectionMode = useAppStore(st => st.slectionMode);
     const onChangeSelectMode = useAppStore(st => st.onChangeSelectMode);
     const onSubmit = useAppStore(st => st.onSubmit);
+    const { trackEvent } = useAptabase();
     return (
         <div className={styles.bottomCenterPanel}>
              <Space>
@@ -95,6 +97,7 @@ function ReactflowBottomCenterPanel() {
                     } else {
                         message.info("Add task to queue");
                     }
+                    trackEvent("comfyui-execute-submit");
                 }}>
                     <StartIcon/>
                 </div>
