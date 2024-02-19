dbe42d3306e8afa34276e7e927d391ab1e2be819
6174 <marcchen955@gmail.com>
2024-02-19 17:05:28 +0800

    add all lang

---
+++ packages/common/i18n/ALL_LANG.ts
@@ -0,0 +1,16 @@
+import { KEYS, i18nAllLang, type i18nLang } from "./i18n-types";
+
+const ALLLang: i18nAllLang = {
+  [KEYS.appName]: {
+    en_US: "comflowy",
+    zh_CN: "comflowy",
+    jp: "comflowy"
+  },
+  [KEYS.myWorkflows]: {
+    en_US: "My Workflows",
+    zh_CN: "我的工作流",
+    jp: "私のワークフロー"
+  },
+};
+
+export default ALLLang;
