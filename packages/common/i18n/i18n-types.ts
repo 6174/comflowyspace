0b5a76ec4589375a4c857566fcae411f7715acad
6174 <marcchen955@gmail.com>
2024-02-20 02:17:01 +0800

    add i18n

--- packages/common/i18n/i18n-types.ts
+++ packages/common/i18n/i18n-types.ts
@@ -1,6 +1,10 @@
 export enum KEYS {
   appName = "appName",
   myWorkflows = "myWorkflows",
+  templates = "templates",
+  tutorials = "tutorials",
+  models = "models",
+  extensions = "extensions"
 }
 
 export type i18nKey = keyof typeof KEYS;
@@ -8,10 +12,11 @@ export type i18nLang = {
   [key in KEYS]: string
 };
 
+export type LanguageType = "zh-CN" | "en-US" | "ja";
 export type i18nAllLang = {
   [key in KEYS]: {
-    en_US: string,
-    zh_CN: string,
-    jp: string
+    'en-US': string,
+    'zh-CN': string,
+    'ja': string
   }
 };
