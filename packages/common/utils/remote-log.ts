796a5a47b014d83456bf794fb1c612b1a673892a
chenxuejia <chenxuejia67@gmail.com>
2024-03-02 17:50:15 +0800

    bugfix

--- packages/common/utils/remote-log.ts
+++ packages/common/utils/remote-log.ts
@@ -7,13 +7,13 @@ export async function remoteLog(props: {
   message: string
 }) {
   try {
-    await fetch("https://www.comflowy.com/api/create-log", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
+    const url = 'http://127.0.0.1:3000/api/create-log';
+    const options = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(props)
-    });
+    }
+    await fetch(url, options);
   } catch(err) {
     console.log("create remote log error:", err);
   }
