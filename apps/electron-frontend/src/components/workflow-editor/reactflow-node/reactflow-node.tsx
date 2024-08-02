4422e20bdbe3720bcd9ee5b03fed8d692c7af6e9
Marc Chen <570171025@qq.com>
2024-08-02 17:39:09 +0800

    optimize slot connecting experience

--- apps/electron-frontend/src/components/workflow-editor/reactflow-node/reactflow-node.tsx
+++ apps/electron-frontend/src/components/workflow-editor/reactflow-node/reactflow-node.tsx
@@ -1,5 +1,5 @@
-import { memo } from 'react'
-import { type NodeProps, Position, Dimensions} from 'reactflow'
+import { memo, useCallback, useState } from 'react'
+import { type NodeProps, Position, Dimensions } from 'reactflow'
 import { Widget, SDNode, PreviewImage, SDNODE_DEFAULT_COLOR, NodeVisibleState } from '@comflowy/common/types';
 import { InputContainer } from '../reactflow-input/reactflow-input-container';
 import nodeStyles from "./reactflow-node.style.module.scss";
@@ -9,11 +9,11 @@ import { getWidgetIcon } from './reactflow-node-icons';
 import { ComfyUINodeError } from '@comflowy/common/types';
 import { getNodeRenderInfo } from "@comflowy/common/workflow-editor/node-rendering";
 import { Slot } from './reactflow-node-slot';
-import { InstallMissingWidget, NodeError } from './reactflow-node-errors';
+import { NodeError } from './reactflow-node-errors';
 import { ComflowyNodeResizer, useNodeAutoResize } from './reactflow-node-resize';
-import { NodeImagePreviews } from './reactflow-node-imagepreviews';
 import { NodeWrapperProps } from './reactflow-node-wrapper';
 import { ReactFlowNodeDynamic } from './reactflow-node-dynamic';
+import { useFullNodeConnecting } from './reactflow-node-slot-full';
 export const NODE_IDENTIFIER = 'sdNode'
 
 interface Props {
@@ -41,10 +41,10 @@ export const NodeComponent = memo(({
   const { inputs, title, outputs, params } = renderInfo;
   const isInProgress = progressBar !== undefined
   const collapsed = node.data.visibleState === NodeVisibleState.Collapsed;
-  const {mainRef, minHeight, minWidth, setResizing} = useNodeAutoResize(node, imagePreviews);
+  const { mainRef, minHeight, minWidth, setResizing } = useNodeAutoResize(node, imagePreviews);
   const transform = useAppStore(st => st.transform || 1);
   const invisible = transform < INVISIBLE_TRANSFORM_THRSHOLD;
-  
+
   let nodeColor = node.data.value.color || SDNODE_DEFAULT_COLOR.color;
   let nodeBgColor = node.data.value.bgcolor || SDNODE_DEFAULT_COLOR.bgcolor;
 
@@ -57,7 +57,13 @@ export const NodeComponent = memo(({
     nodeBgColor = "#261E1F";
     nodeColor = "#DE654B";
   }
-  
+
+  if (isInProgress) {
+    console.log("node-progress", progressBar, node.data.value.widget)
+  }
+
+  const { connectingIndicator, onMouseEnter, onMouseUp, onMouseLeave, onMouseMove } = useFullNodeConnecting({ widget: widget.name, node_id: node.id, inputs, outputs });
+
   return (
     <div className={`
       ${nodeStyles.reactFlowNode}
@@ -72,23 +78,28 @@ export const NodeComponent = memo(({
         '--node-color': nodeColor,
         '--node-border-color': nodeColor,
         'opacity': renderInfo.enabled ? 0.5 : 1,
-        '--node-bg-color': (isInProgress || !!nodeError) ? nodeBgColor : Color(nodeBgColor).alpha(.95).hexa(),
-    } as React.CSSProperties}>
+        '--node-bg-color': (isInProgress || !!nodeError) ? nodeBgColor : Color(nodeBgColor).alpha(.92).hexa(),
+      } as React.CSSProperties}
+      onMouseEnter={onMouseEnter}
+      onMouseUp={onMouseUp}
+      onMouseLeave={onMouseLeave}
+      onMouseMove={onMouseMove}
+    >
 
-      {!collapsed && <ComflowyNodeResizer setResizing={setResizing} minWidth={minWidth} minHeight={minHeight} node={node} /> }
+      {!collapsed && <ComflowyNodeResizer setResizing={setResizing} minWidth={minWidth} minHeight={minHeight} node={node} />}
 
       {!invisible ? (
         <div className={`node-inner ${widget.input.required.upload ? "LoadImage" : ""}`}>
           <div className="node-header">
             <h2 className="node-title" style={getTransformStyle(transform)}>
-              {getWidgetIcon(widget)} 
-                {title}
-                {isPositive && <span>{"("}Positive{")"}</span>} 
-                {isNegative && <span>{"("}Negative{")"}</span>} 
-                <NodeError nodeError={nodeError}/>
+              {getWidgetIcon(widget)}
+              {title}
+              {isPositive && <span>{"("}Positive{")"}</span>}
+              {isNegative && <span>{"("}Negative{")"}</span>}
+              <NodeError nodeError={nodeError} />
             </h2>
 
-            {isInProgress? 
+            {isInProgress ?
               <div className="progress-bar">
                 <div className="progress-indicator" style={{
                   width: `${progressBar * 100}%`
@@ -102,7 +113,7 @@ export const NodeComponent = memo(({
               <div className="node-slots">
                 <div className="node-inputs">
                   {inputs.map((input, index) => (
-                    <Slot node_id={node.id}  key={input.name + index} widget={widget.name} valueType={input.type} id={input.name} label={input.name} type="target" position={Position.Left} />
+                    <Slot node_id={node.id} key={input.name + index} widget={widget.name} valueType={input.type} id={input.name} label={input.name} type="target" position={Position.Left} />
                   ))}
                 </div>
                 <div className="node-outputs">
@@ -116,22 +127,22 @@ export const NodeComponent = memo(({
                   <>
                     <div className="node-params">
                       {params.map(({ property, input }) => (
-                        <InputContainer env={"main"} key={property} name={property} id={node.id} node={node.data.value} input={input} widget={widget} />
+                        <InputContainer env="main" key={property} name={property} id={node.id} node={node.data.value} input={input} widget={widget} />
                       ))}
                     </div>
-                    <InstallMissingWidget nodeError={nodeError} node={node.data.value} />
                     <div style={{ height: 10 }}></div>
                   </>
                 )
               }
             </div>
-            {!collapsed && <ReactFlowNodeDynamic renderInfo={renderInfo} node={node}  imagePreviews={imagePreviews}/>}
+            {!collapsed && <ReactFlowNodeDynamic renderInfo={renderInfo} node={node} imagePreviews={imagePreviews} />}
           </div>
+          {connectingIndicator}
         </div>
       ) : (
         <>
-          <div className="node-header"></div>
-          <div className="node-main"></div>
+          <div className="node-header" style={{ visibility: "hidden" }}></div>
+          <div className="node-main" style={{ visibility: "hidden" }}></div>
         </>
       )}
     </div>
